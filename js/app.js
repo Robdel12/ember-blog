8ec260b26ab4b032a1f78876ab1e9495cc18875c
Robert DeLuca <robertd@izea.com>
2014-07-21 14:56:56 -0400

    Added disqus comments to posts

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ App.IndexRoute = Ember.Route.extend({
 App.BlogRoute = Ember.Route.extend({
   model: function(){
     return this.store.findAll("post");
+    // return post;
   }
 });
 
@@ -42,7 +43,7 @@ App.BlogController = Ember.ArrayController.extend({
 
 App.NewController = Ember.ObjectController.extend({
 
-  isLoggedIn: false,
+  isLoggedIn: true,
 
   init: function() {
     this.set('post',  Ember.Object.create());
@@ -115,4 +116,30 @@ Ember.Handlebars.helper('markdown', function(value, options) {
 
 Ember.Handlebars.helper('copyrightDate', function(){
   return new Date().getFullYear();
+});
+
+App.CDisqusComponent = Em.Component.extend({
+  didInsertElement: function () {
+    var page_id = window.location.href,
+        disqus_identifier = page_id,
+        disqus_url = page_id,
+        disqus_title = Em.$('title').text(),
+        disqus_shortname = 'robertdeluca', // CHANGE, USE YOURS
+        el_id = disqus_shortname + Date.now();
+
+        console.log(page_id);
+        console.log(disqus_title);
+
+    this.set('page_id', el_id);
+
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    dsq.id = el_id;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  },
+  willDestroyElement: function () {
+      Em.$('#' +  this.get('page_id')).remove();
+  }
 });
